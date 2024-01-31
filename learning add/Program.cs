5186ac4a30aebaf9cc089a0e2f5c830a0faf7705
domic1 <115622468+domic1@users.noreply.github.com>
2024-01-31 21:37:52 +0100

    Dodaj pliki projektów.

---
+++ learning add/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Done_is_better_than_good
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random zmienna = new Random();
+            int suma, wynik;
+            int a = zmienna.Next(10, 100);
+            int b = zmienna.Next(10, 100);
+            Console.WriteLine("Cześć, dodaj niżej skazane cyfry");
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);   
+            suma = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (a+b==suma)
+            {
+                Console.WriteLine("Dodałeś poprawnie liczby");
+            }
+            else
+            {
+                wynik = a + b;
+                Console.Write("Dodałeś błędnie liczby. Poprawny wynik to:" + wynik  );
+            }
+        }
+    }
+}
